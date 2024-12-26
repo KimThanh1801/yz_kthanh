103fca3ef33a3633016a27ba8ffd588bf3ea8faa
KimThanh1801 <hokimthanh1234@gmail.com>
2024-12-27 01:58:11 +0700

    interface header

---
+++ app/views/shared/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../assets/css/header.css">
+</head>
+<body>
+    <header>
+        <div class="container mt-2">
+            <div class="d-flex justify-content-between align-items-center">
+                <ul class="nav nav-pills">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            PRODUCTS
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Animal Statues</a></li>
+                            <li><a class="dropdown-item" href="#">Coconut Liners</a></li>
+                            <li><a class="dropdown-item" href="#">Metal Planters</a></li>
+                            <li><a class="dropdown-item" href="#">Outdoor Decor</a></li>
+                            <li><a class="dropdown-item" href="#">Small Indoor Pots</a></li>
+                            <li><a class="dropdown-item" href="#">Watering Bags</a></li>
+                            <li><a class="dropdown-item" href="#">Wrought Iron</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ABOUT</a>
+                    </li>
+                </ul>
+                <div class="me-3 nav">
+                    <img src="../../../assets/image/logo.png" alt="Logo" style="height: 40px;">
+                </div>
+
+                <ul class="nav nav-pills ms-3">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item li-center">
+                        <a class="nav-link" href="#">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            <span>0 ITEM - $0</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            MY ACCOUNT
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
