a497ec47e24b0893ecfb11aa3508e75410a3be04
KimThanh1801 <hokimthanh1234@gmail.com>
2025-01-08 11:57:10 +0700

    update add color for menu-con of header

--- app/shared/header.php
+++ app/shared/header.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../assets/css/header.css">
-</head>
-<body>
-    <header>
-        <div class="container mt-2">
+<header>
+        <div class="container mt-0">
             <div class="d-flex justify-content-between align-items-center">
                 <ul class="nav nav-pills">
                     <li class="nav-item dropdown">
@@ -39,7 +24,7 @@
                     </li>
                 </ul>
                 <div class="me-3 nav">
-                    <img src="../../assets/image/logo.png" alt="Logo" style="height: 40px;">
+                    <img src="\yz_kthanh\assets\image\logo.png" alt="Logo" style="height: 40px;">
                 </div>
 
                 <ul class="nav nav-pills ms-3">
@@ -63,8 +48,3 @@
             </div>
         </div>
     </header>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
